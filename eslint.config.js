7e12043d6cbec2ce5af7141ba8ec308cbc770693
roc <lipeng1080@gmail.com>
2026-02-08 14:42:44 +0800

    style: add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'dist/**', '.umi/**', '.umi-production/**'],
+  },
+  {
+    rules: {
+      'no-console': 'off',
+      'no-undef': 'off',
+    },
+  },
+];
